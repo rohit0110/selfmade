d54cbee370ba660d8fbc6b4cbbd9de199670721b
rohit0110 <rohit7rathore15@gmail.com>
2026-05-07 18:15:08 +0530

    feat: add EXPIRE and TTL

--- redis/src/commands.rs
+++ redis/src/commands.rs
@@ -57,8 +57,28 @@ pub fn handle(resp_value: RespValue, store: Arc<Mutex<Store>>) -> RespValue {
                                 _ => continue
                             });
                         }
-                        let store = store.lock().unwrap();
+                        let mut store = store.lock().unwrap();
                         return store.mget(keys);
+                    },
+                    "EXPIRE" => {
+                        let mut store = store.lock().unwrap();
+                        match &elements[1] {
+                            RespValue::BulkString(Some(key)) => match &elements[2] {
+                                RespValue::BulkString(Some(ttl)) => return store.expire(key, match ttl.parse::<i64>() {
+                                    Some(val) => val,
+                                    _ => return RespValue::Error(String::from("TIME PROVIDED IS NOT A NUMBER")),
+                                }), //what if not i64 parsed?
+                                _ => return RespValue::Error(String::from("NO TTL PROVIDED")),
+                            },
+                            _ => RespValue::Error(String::from("NO KEY PROVIDED"))
+                        }
+                    },
+                    "TTL" => {
+                        let mut store = store.lock().unwrap();
+                        match &elements[1] {
+                            RespValue::BulkString(Some(key)) => return store.ttl(key),
+                            _ => return RespValue::Error(String::from("NO KEY PROVIDED"))
+                        }
                     }
                     _ => RespValue::Error(String::from("ERR unknown command")),                                  
                 },                                                                                               
--- redis/src/store.rs
+++ redis/src/store.rs
@@ -1,8 +1,23 @@
 use std::collections::HashMap;
+use std::time::{Duration, Instant};
 use crate::resp::RespValue;
 
 pub struct Store {
-    store: HashMap<String,String>
+    store: HashMap<String,StoreValue>
+}
+
+struct StoreValue {
+    value: String,
+    expires_at: Option<Instant>
+}
+
+impl StoreValue {
+    fn new(value: String) -> Self {
+        Self {
+            value,
+            expires_at: None
+        }
+    }
 }
 
 impl Store {
@@ -12,20 +27,29 @@ impl Store {
         }
     }
 
-    pub fn get(&self,key: &str) -> RespValue {
+    pub fn get(&mut self,key: &str) -> RespValue {
         if self.store.contains_key(key) {
-             return RespValue::BulkString(Some(self.store.get(key).unwrap().clone()));
+            if self.has_expired(key) {
+                self.store.remove(key);
+                return RespValue::BulkString(None);
+            }
+            return RespValue::BulkString(Some(self.store.get(key).unwrap().value.clone()));
         }
         else {
             return RespValue::BulkString(None);
         }
     }
 
-    pub fn mget(&self, keys: Vec<&str>) -> RespValue {
+    pub fn mget(&mut self, keys: Vec<&str>) -> RespValue {
         let mut resp = vec![];
         for key in keys {
             if self.store.contains_key(key) {
-                resp.push(RespValue::BulkString(Some(self.store.get(key).unwrap().clone())));
+                if self.has_expired(key) {
+                    self.store.remove(key);
+                    resp.push(RespValue::BulkString(None));
+                } else {
+                    resp.push(RespValue::BulkString(Some(self.store.get(key).unwrap().value.clone())));
+                }
             }
             else {
                 resp.push(RespValue::BulkString(None));
@@ -35,7 +59,7 @@ impl Store {
     }
 
     pub fn set(&mut self, key: &str, val: &str) -> RespValue {
-        self.store.insert(key.to_string(), val.to_string());
+        self.store.insert(key.to_string(), StoreValue::new(val.to_string()));
         return RespValue::SimpleString(String::from("OK"));
     }
 
@@ -48,8 +72,12 @@ impl Store {
         }
     }
 
-    pub fn exists(&self, key: &str) -> RespValue {
+    pub fn exists(&mut self, key: &str) -> RespValue {
         if self.store.contains_key(key) {
+            if self.has_expired(key) {
+                self.store.remove(key);
+                return RespValue::Integer(0);
+            }
             return RespValue::Integer(1);
         } else {
             return RespValue::Integer(0);
@@ -57,16 +85,54 @@ impl Store {
     } 
 
     pub fn incr(&mut self, key: &str) -> RespValue {
-        if self.store.contains_key(key) {
-            let mut val = match self.store.get(key).clone().unwrap().parse::<i64>() {
+        if self.store.contains_key(key) && !self.has_expired(key) {
+            let val = match self.store.get(key).unwrap().value.clone().parse::<i64>() {
                 Ok(n) => n+1,
                 Err(_) => return RespValue::Error(String::from("VALUE IS NOT A NUMBER"))
             };
-            self.store.insert(key.to_string(), val.to_string());
+            self.store.insert(key.to_string(), StoreValue::new(val.to_string()));
             return RespValue::Integer(val);
         } else {
-            self.store.insert(key.to_string(), 1.to_string());
+            self.store.insert(key.to_string(), StoreValue::new(1.to_string()));
             return RespValue::Integer(1);
+        }
+    }
+
+    pub fn expire(&mut self, key: &str, seconds: i64) -> RespValue {
+        if self.store.contains_key(key) && !self.has_expired(key) {
+            if let Some(store_value) = self.store.get_mut(key) {
+                store_value.expires_at = Some(Instant::now() + Duration::from_secs(seconds as u64));
+                return RespValue::Integer(1);
+            } else {
+                return RespValue::Error(String::from("Issue with setting expiry"));
+            }
+        } else {
+            return RespValue::Integer(0);
+        }
+    }  
+
+    pub fn ttl(&mut self, key:&str) -> RespValue {
+        if self.store.contains_key(key) && !self.has_expired(key) {
+            let expires_at = match self.store.get(key).unwrap().expires_at {
+                Some(time) => time,
+                None => return RespValue::Integer(-1),
+            };
+            return RespValue::Integer((expires_at.duration_since(Instant::now()).as_secs()) as i64);
+        } else {
+            if self.store.contains_key(key) {
+                self.store.remove(key);
+            }
+            return RespValue::Integer(-2);
+        }
+    }
+
+    fn has_expired(&self, key: &str) -> bool {
+        match self.store.get(key) {
+            Some(val) => match val.expires_at {
+                Some(time) => Instant::now() > time,
+                None => return false
+            },
+            None => return false
         }
     }
 }
